2580e7b90b43bc9fb9fa6f7f5286c174e2a461cc
Vadym Zaiats <vadyk.za@gmail.com>
2023-09-25 16:29:34 +0300

    burger page fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -8,32 +8,30 @@ open.addEventListener("click", (e) => {
   close.classList.remove("hide");
   header.classList.add("burger-active");
   burgerMenu.classList.remove("hide");
-  //   document.querySelector(".header__nav--list").classList.add("active");
 });
 
 document.querySelector(".close").addEventListener("click", (e) => {
   e.target.classList.add("hide");
   open.classList.remove("hide");
-  header.classList.add("burger-active");
+  header.classList.remove("burger-active");
   burgerMenu.classList.add("hide");
-  // document.querySelector(".header__nav--list").classList.remove("active");
 });
 
-const burger = document.querySelector(".header__nav--list");
-document.addEventListener("click", (e) => {
-  if (
-    !e.composedPath().includes(burger) &&
-    burger.classList.contains("active") &&
-    !e.target.classList.contains("burger")
-  ) {
-    document.querySelector(".header__nav--list").classList.remove("active");
-    open.classList.remove("hide");
-    close.classList.add("hide");
-  }
-});
+// const burger = document.querySelector(".header__nav--list");
+// document.addEventListener("click", (e) => {
+//   if (
+//     !e.composedPath().includes(burger) &&
+//     burger.classList.contains("active") &&
+//     !e.target.classList.contains("burger")
+//   ) {
+//     document.querySelector(".header__nav--list").classList.remove("active");
+//     open.classList.remove("hide");
+//     close.classList.add("hide");
+//   }
+// });
 
-window.addEventListener("resize", () => {
-  document.querySelector(".header__nav--list").classList.remove("active");
-  open.classList.remove("hide");
-  close.classList.add("hide");
-});
+// window.addEventListener("resize", () => {
+//   document.querySelector(".header__nav--list").classList.remove("active");
+//   open.classList.remove("hide");
+//   close.classList.add("hide");
+// });
