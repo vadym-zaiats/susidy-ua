4ea456897953874bd707b516be7b7e332630c80e
Vadym Zaiats <vadyk.za@gmail.com>
2023-10-04 12:45:39 +0300

    form logic in process

--- src/js/form.js
+++ src/js/form.js
@@ -1,25 +1,67 @@
-let currentPage = 1;
+let flat;
+let accessoryType;
+let buyerData;
+const flatType = document.querySelector(".flat-type");
+const flatAccessories = document.querySelector(".accessories-flat");
+const balconyAccessories = document.querySelector(".accessories-balcony");
+const terraceAccessories = document.querySelector(".accessories-terrace");
 
-function nextPage() {
-  document.getElementById(`page${currentPage}`).style.display = "none";
-  currentPage++;
-  document.getElementById(`page${currentPage}`).style.display = "block";
+function nextPage(event) {
+  const roomType = event.target.value;
+  switch (roomType) {
+    case "до 200 грн":
+      flat = roomType;
+      flatType.classList.add("flat-type--hide");
+      flatAccessories.classList.remove("accessories-flat--hide");
+      break;
+    case "до 250 грн":
+      flat = roomType;
+      flatType.classList.add("flat-type--hide");
+      balconyAccessories.classList.remove("accessories-balcony--hide");
+      break;
+    case "до 500 грн":
+      flat = roomType;
+      flatType.classList.add("flat-type--hide");
+      terraceAccessories.classList.remove("accessories-terrace--hide");
+      break;
+  }
 }
 
-function submitForm(event) {
-  event.preventDefault(); // Prevent form submission
+// function nextFurniturePage(furnitureType) {
+//   nextPage();
 
-  // Collect customer data
-  const customerData = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    roomChoice: document.querySelector('input[name="roomType"]:checked').value,
-    furnitureChoice: document.querySelector(
-      'input[name="furnitureType"]:checked'
-    ).value,
-  };
+//   // Additional logic specific to the furniture type can be added here
+// }
 
-  // TODO: Send customerData to your backend using an AJAX request
+// function submitForm(event) {
+//   event.preventDefault(); // Prevent form submission
 
-  console.log("Customer Data:", customerData); // For demonstration purposes, log the data
-}
+//   // Collect customer data
+//   const customerData = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     roomChoice: document.querySelector('input[name="roomType"]:checked').value,
+//     furnitureChoice: getSelectedFurniture(),
+//   };
+
+//   // TODO: Send customerData to your backend using an AJAX request
+
+//   console.log("Customer Data:", customerData); // For demonstration purposes, log the data
+// }
+
+// function getSelectedFurniture() {
+//   const roomType = document.querySelector(
+//     'input[name="roomType"]:checked'
+//   ).value;
+//   const furnitureOptions = document.querySelectorAll(
+//     `input[name="furnitureType${capitalizeFirstLetter(roomType)}"]:checked`
+//   );
+//   const selectedFurniture = Array.from(furnitureOptions).map(
+//     (option) => option.value
+//   );
+//   return selectedFurniture;
+// }
+
+// function capitalizeFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
