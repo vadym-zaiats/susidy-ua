9d3fe6f020f363aecb1a8cb158d00e71ac614268
Vadym Zaiats <vadyk.za@gmail.com>
2023-10-04 11:10:48 +0300

    some scss edited

--- src/js/form.js
+++ src/js/form.js
@@ -1,1 +1,25 @@
+let currentPage = 1;
 
+function nextPage() {
+  document.getElementById(`page${currentPage}`).style.display = "none";
+  currentPage++;
+  document.getElementById(`page${currentPage}`).style.display = "block";
+}
+
+function submitForm(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Collect customer data
+  const customerData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    roomChoice: document.querySelector('input[name="roomType"]:checked').value,
+    furnitureChoice: document.querySelector(
+      'input[name="furnitureType"]:checked'
+    ).value,
+  };
+
+  // TODO: Send customerData to your backend using an AJAX request
+
+  console.log("Customer Data:", customerData); // For demonstration purposes, log the data
+}
