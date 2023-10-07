e175bd17ae8f207c58d5a238bb017adfd7ed205b
Vadym Zaiats <vadyk.za@gmail.com>
2023-10-07 14:07:47 +0300

    form ref

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,9 @@
 let flat = null;
 let accessoryType = null;
 let buyerData = null;
+
+const form = document.querySelector(".general__form");
+
 const flatType = document.querySelector(".flat-type");
 
 const buyerPage = document.querySelector(".buyer");
@@ -16,37 +19,56 @@ const terraceAccessories = document.querySelector(".accessories-terrace");
 const buyerBackText = document.querySelector(".buyer__back-text");
 const donateSum = document.querySelector(".buyer__sum");
 
-const form = document.querySelector(".general__form");
-
 const certificatePage = document.querySelector(".certificate");
 const qwe = document.querySelector(".certificate__button");
 
-function toAccessoriesPage(event) {
-  const roomType = event.target.value;
+form.addEventListener("click", (event) => {
+  event.preventDefault();
 
-  switch (roomType) {
-    case "до 200 грн":
-      flat = roomType;
+  const eventTargetValue = event.target.value;
+  const eventTargetParent = event.target.parentElement;
+  // console.log(eventTargetValue);
+  // console.log(eventTargetParent);
+
+  switch (eventTargetValue) {
+    case "flat":
+      flat = eventTargetValue;
       flatType.classList.add("flat-type--hide");
       flatAccessories.classList.remove("accessories-flat--hide");
+      donateSum.min = 200;
+      donateSum.setAttribute("placeholder", "Сума донату (не менше 200 грн)");
+      buyerBackText.innerText = "Придбати квартиру з балконом та котиком";
       break;
-    case "до 250 грн":
-      flat = roomType;
+    case "balcony":
+      flat = eventTargetValue;
       flatType.classList.add("flat-type--hide");
       balconyAccessories.classList.remove("accessories-balcony--hide");
+      donateSum.min = 250;
+      donateSum.setAttribute("placeholder", "Сума донату (не менше 250 грн)");
+      buyerBackText.innerText = "Придбати квартиру з функціональною терасою";
       break;
-    case "до 500 грн":
-      flat = roomType;
+    case "terrace":
+      flat = eventTargetValue;
       flatType.classList.add("flat-type--hide");
       terraceAccessories.classList.remove("accessories-terrace--hide");
+      donateSum.min = 500;
+      donateSum.setAttribute("placeholder", "Сума донату (не менше 500 грн)");
+      buyerBackText.innerText = "Придбати іншу квартиру";
       break;
   }
-}
+
+  if (eventTargetParent.classList.contains("accessories-styles__label")) {
+    accessoryType = eventTargetValue;
+    accessoriesItems.forEach((item) => {
+      if (!item.classList.contains(`${item.id}--hide`)) {
+        item.classList.add(`${item.id}--hide`);
+      }
+    });
+  }
+});
 
 function toBuyerData(event) {
   event.preventDefault();
-
-  accessoryType = event.target.value;
 
   accessoriesItems.forEach((item) => {
     if (!item.classList.contains(`${item.id}--hide`)) {
@@ -75,42 +97,42 @@ function toBuyerData(event) {
   }
 }
 
-function backToFlatType(event) {
-  event.preventDefault();
+// function backToFlatType(event) {
+//   event.preventDefault();
 
-  accessoriesItems.forEach((item) => {
-    if (!item.classList.contains(`${item.id}--hide`)) {
-      item.classList.add(`${item.id}--hide`);
-    }
-  });
+//   accessoriesItems.forEach((item) => {
+//     if (!item.classList.contains(`${item.id}--hide`)) {
+//       item.classList.add(`${item.id}--hide`);
+//     }
+//   });
 
-  buyerPage.classList.add("buyer--hide");
-  flatType.classList.remove("flat-type--hide");
+//   buyerPage.classList.add("buyer--hide");
+//   flatType.classList.remove("flat-type--hide");
 
-  flat = null;
-  accessoryType = null;
-}
+//   flat = null;
+//   accessoryType = null;
+// }
 
-function submitForm(event) {
-  event.preventDefault();
-  buyerData = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    email: document.getElementById("balconyText").value,
-    donate: document.getElementById("donationAmount").value,
-  };
-  buyerPage.classList.add("buyer--hide");
-  thankYouPagePage.classList.remove("thank-you-page--hide");
-  console.log(flat, accessoryType, buyerData);
-}
-form.addEventListener("submit", submitForm);
+// function submitForm(event) {
+//   event.preventDefault();
+//   buyerData = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     email: document.getElementById("balconyText").value,
+//     donate: document.getElementById("donationAmount").value,
+//   };
+//   buyerPage.classList.add("buyer--hide");
+//   thankYouPagePage.classList.remove("thank-you-page--hide");
+//   console.log(flat, accessoryType, buyerData);
+// }
+// form.addEventListener("submit", submitForm);
 
-function toCertificatePage() {
-  thankYouPagePage.classList.add("thank-you-page--hide");
-  certificatePage.classList.remove("certificate--hide");
-}
+// function toCertificatePage() {
+//   thankYouPagePage.classList.add("thank-you-page--hide");
+//   certificatePage.classList.remove("certificate--hide");
+// }
 
-function downloadCertificate(event) {
-  event.preventDefault();
-  alert("Тут має початись скачування сертифікату");
-}
+// function downloadCertificate(event) {
+//   event.preventDefault();
+//   alert("Тут має початись скачування сертифікату");
+// }
