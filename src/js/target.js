e97bbb7b4a6c165c86a4ca9814045473530bc749
Roman Marchenko <kirkusik@gmail.com>
2023-11-28 18:50:14 +0200

    ch

--- src/js/target.js
+++ src/js/target.js
@@ -1,9 +1,14 @@
-const terget = 10_000_000;
+const terget = document
+	.querySelector(".purpose__bar")
+	.getAttribute("data-target");
+
 const alreadyHave = document
-  .querySelector(".purpose__bar")
-  .getAttribute("data-fill");
+	.querySelector(".purpose__bar")
+	.getAttribute("data-fill");
+
 const fillElement = document.querySelector(".purpose__fill");
 const countElement = document.querySelector(".purpose__count");
+
 const percentage = alreadyHave / (terget / 100);
 fillElement.style.width = `${percentage}%`;
 countElement.textContent = `${alreadyHave} / ${terget}`;
